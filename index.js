1683fa9cb4b3831e4a3ea7509fc801bee4c750c5
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 10:24:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here!
 
-let bo = document.getElementsByTagName('main')
-bo.removeChild()
+let bo = document.getElementsByTagName('body')
+bo.removeChild(bo.querySelector('main#main'))
 //let main = document.getElementById("main#main");
 //removeChild(querySelector('main#main'));
 //main.remove();
