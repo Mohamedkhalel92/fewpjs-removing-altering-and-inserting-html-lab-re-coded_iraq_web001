51838fbbc09d4e6c43e77f2788cdb571f255d7d1
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 10:12:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 let element = querySelector('main#main');
 element.remove();
-ul.removeChild(ul.querySelector('li:nth-child(2)'))
+ul.removeChild(ul.querySelector('main#main'))
