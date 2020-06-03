2721449ee649e84d01d3891c53936734e0994bd2
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 11:34:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+main.remove();
+//let h1 = createElement("h1.victory")
+let h1 = document.createElement('h1')
+h1.setAttribute("id", "victory");
+let bo = document.getElementsByTagName('body');
+bo[0].appendChild(h1);
+var newHeader = document.getElementById('victory');
+//let newHeader ;
+newHeader.innerHTML ="Myname is the champion";
