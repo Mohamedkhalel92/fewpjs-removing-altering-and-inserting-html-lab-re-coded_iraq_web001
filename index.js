ed07a626207cf7d1c365bd0802373b16958bcb72
mohamed khalel <mohamed.khalel.1992.1992@gmail.com>
2020-06-03 10:30:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code here!
-
 let bo = document.getElementsByTagName('body')
-bo.removeChild(bo.querySelector('main'))
+bo.removeChild(bo.querySelector('main#main'))
 //let main = document.getElementById("main#main");
 //removeChild(querySelector('main#main'));
 //main.remove();
